338275014132ae87e5fa94aaeafb610bca50d959
ingvar73 <ingvar73@gmail.com>
2016-09-12 02:34:19 +0300

    Last commit -3

--- app/controllers/controller_login.php
+++ app/controllers/controller_login.php
@@ -23,7 +23,7 @@ class Controller_Login extends Controller {
             $reg->regex(Model_Auth::M_PASSWORD_PATTERN, $reg->password, 'Некорректный пароль!');
             $reg->regex(Model_Auth::LOGIN_PATTERN, $reg->login, 'Некорректный логин!');
 
-            $result = $db->query("SELECT id, password FROM users WHERE login = '{$reg->login}'");
+            $result = $db->query("SELECT id, email, password FROM users WHERE login = '{$reg->login}'");
             $row = $db->fetch_assoc($result);
 //            var_dump($row['password']);
 //            var_dump($reg->password);
@@ -37,6 +37,7 @@ class Controller_Login extends Controller {
                     setcookie("id", $row['id'], time()+60*60*24*30);
                     $_SESSION["login"] = $login;
                     $_SESSION["password"] = $password;
+                    $_SESSION["email"] = $row['email'];
 
                 } else{
                     echo "Пользователь не существует, зарегистрируйтесь";
--- app/models/model_user.php
+++ app/models/model_user.php
@@ -1,17 +1,26 @@
 <?php
-
+require_once __DIR__.'/../components/Db.php';
 class Model_User extends Model {
 
     public function get_data()
     {
+        session_start();
+        $db = Db::getInstance();
+
+        if (!empty($_SESSION['login']) and !empty($_SESSION['password'])) {
+            //если существует логин и пароль в сессиях, то проверяем их
+            $login = $_SESSION['login'];
+            $password = $_SESSION['password'];
+            $result = $db->query("SELECT id, login, email FROM users WHERE login='$login'");
+            $myrow = $result->fetch_assoc();
+            var_dump($myrow);
         return array(
             array(
-                "name" => "",
-                "email" => "",
-                "about" => "",
-                "avatar" => ""
-            )
-        );
+                "login" => $myrow['login'],
+                "email" => $myrow['email'],
+                )
+            );
+        }
     }
 
 }
